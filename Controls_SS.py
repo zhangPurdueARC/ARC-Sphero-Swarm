da2f8c9eb140f846dc4218f71e86192af2b7693b
zhangPurdueARC <zhan4833@purdue.edu>
2024-02-04 15:48:40 -0500

    Update - Multiple Balls Manual

--- Controls_SS.py
+++ Controls_SS.py
@@ -2,9 +2,10 @@ from spherov2 import scanner
 from spherov2.sphero_edu import SpheroEduAPI
 
 # Find toys
-toy = scanner.find_toy(toy_name = "SB-76B3")
+toys = scanner.find_toys(toy_names = ["SB-76B3", "SB-B11D"])
 
-print(toy)
+print(toys)
 
-with SpheroEduAPI(toy) as api:
-    api.roll(0, 255, 1)
+for toy in toys:
+    with SpheroEduAPI(toy) as api:
+        api.roll(0, 255, 1)
