073a1f6c44fb3bf2d0d99a661902224e25e2d891
zhangPurdueARC <zhan4833@purdue.edu>
2024-02-04 16:31:12 -0500

    String Control System Prototype

--- Controls_SS.py
+++ Controls_SS.py
@@ -3,18 +3,30 @@ from spherov2.sphero_edu import SpheroEduAPI
 import threading
 import time
 
-def rotate_toy(toy):
+def control_toy(toy, id):
+    print(id)
     with SpheroEduAPI(toy) as api:
-        api.roll(0, 255, 1)
+        while commands[id] != "":    
+            api.roll(0, 255, 1)
+            print(commands[id])
+            commands[id] = commands[id][1:]
+            time.sleep(0.1)
 
 def run_toy_threads(toys):
     threads = []
+    global commands 
+    commands = []
     for toy in toys:
-        thread = threading.Thread(target=rotate_toy, args=[toy])
+        commands.append("abcd")
+    id = 0
+    for toy in toys:
+        thread = threading.Thread(target=control_toy, args=[toy, id])
         threads.append(thread)
         thread.start()
+        id += 1
     for thread in threads:
         thread.join()
+    print("Ending function...")
 
 # Find toys
 toys = scanner.find_toys(toy_names = ["SB-76B3", "SB-B11D"])
