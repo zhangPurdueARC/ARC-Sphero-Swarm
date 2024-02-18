ebd9805d9291d200633f19b90d8169e1b37c1659
zhangPurdueARC <zhan4833@purdue.edu>
2024-02-18 16:16:30 -0500

    Compass fixed

--- Controls_SS.py
+++ Controls_SS.py
@@ -4,65 +4,94 @@ from spherov2.types import Color
 import threading
 import time
 
-def control_toy(toy, id, commands): # need to make a new system sooner or later - just working on bare threads right now
+def toy_manager(toy, id):
+    global recieveNew
+    global commands
     with SpheroEduAPI(toy) as api:
-        choosenColor = Color(r = 255, g = 0, b = 0)
-        api.set_front_led(choosenColor)
-        api.set_back_led(choosenColor)
-        # still needs more work (untested, also probably doesn't work on matrix specific to the BOLT)
-        numIterations = 0
-        while True or commands == []:    
-            print("{}: {}".format(id, commands[0]))
-            if (commands[0] == "%"):
-                allReady[id] = 1
-                break
-            elif (commands[0] == "c"):
-                api.calibrate_compass()
-                api.set_compass_direction(0)
-                pass
-            elif (commands[0] == "m"):
-                api.roll(0, 255, 0.5)
-                time.sleep(0.5)
-            elif (commands[0] == "r"):
-                api.spin(90, 1) 
-            # probably need to graph sphero turning in order to get a good idea of what the rotate command should be
+        while True:
+            if recieveNew == True:
+                recieveNew = False
+                control_toy(api, id, commands[id])
             else:
-                print(commands[0] + " in ball {} is an invalid command!".format(id))
-            commands = commands[1:]
-            allReady[id][numIterations] = 1
-            while True:
-                ready = True
-                for readiness in allReady:
-                    if (readiness[numIterations] == 0):
-                        ready = False
-                        break
-                if (ready):
-                    break
-            numIterations += 1
-            
-def run_toy_threads(toys):
-    threads = []
-    commands = []
+                exit() # temp measure to prevent infinite loop
+
+def control_toy(api, id, command):
     global allReady
+    choosenColor = Color(r = 255, g = 0, b = 0)
+    api.set_main_led(choosenColor)
+    api.set_front_led(choosenColor)
+    api.set_back_led(choosenColor)
+    direction = 0
+    # doesn't work on matrix, but does work on the positional lights
+    numIterations = 0
+    while True and command != []:
+        print("{}: {}".format(id, command[0]))
+        if (command[0] == "%"):
+            allReady[id] = 1
+            break
+        elif (command[0] == "c"):
+            api.calibrate_compass()
+            api.set_main_led(choosenColor)
+        elif (command[0] == "m"):
+            api.roll(api.get_compass_direction() + direction, 255, 0.5)
+            time.sleep(0.5)
+        elif (command[0] == "r"):
+            direction += 90
+        # probably need to graph sphero turning in order to get a good idea of what the rotate command should be
+        else:
+            print(command[0] + " in ball {} is an invalid command!".format(id))
+        command = command[1:]
+        allReady[id][numIterations] = 1
+        while True:
+            ready = True
+            for readiness in allReady:
+                if (readiness[numIterations] == 0):
+                    ready = False
+                    break
+            if (ready):
+                break
+        numIterations += 1
+        time.sleep(1)
+
+def commandInputs(toys): # needs to be able to consistently take in data
+    global commands
+    global allReady
+    
+    commands = []
     allReady = []
-    command = "cmmsr%"
+
+    command = "cmrm%"
     for toy in toys:
         commands.append(command) # matrix is needed for more complex commands
         allReady.append([0] * len(command))
+            
+def run_toy_threads(toys):
     id = 0
+    threads = []
+    
+    global commands
+    global allReady
+    global recieveNew
+
+    recieveNew = True
+
+    commandInputs(toys)
+
     for toy in toys:
-        thread = threading.Thread(target=control_toy, args=[toy, id, commands[id]])
+        thread = threading.Thread(target=toy_manager, args=[toy, id])
         threads.append(thread)
         thread.start()
         id += 1
-    # while True: need to recieve commands in constantly updating format
+    
     for thread in threads:
         thread.join()
     print("Ending function...")
 
 # Find toys
-toys = scanner.find_toys(toy_names = ["SB-76B3", "SB-B11D"])
-
+toys = scanner.find_toys(toy_names = ["SB-76B3"]) # "SB-B11D"
 print(toys)
 
-run_toy_threads(toys)
+try:
+    run_toy_threads(toys)
+except KeyboardInterrupt:
+    print("Force terminating function.")
