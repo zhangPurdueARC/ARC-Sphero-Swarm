97c5fea63ac313bf65518bb1a948876836635756
zhangPurdueARC <zhan4833@purdue.edu>
2024-10-13 16:35:02 -0400

    Saved to personal git

--- functionalities.py
+++ functionalities.py
@@ -5,9 +5,9 @@ import matplotlib.pyplot as plt
 import csv
 
 # values for operation
-toy_name = scanner.find_toy(toy_name = "SB-B11D")
+toy_name = scanner.find_toy(toy_name = "SB-76B3")
 speed = int(input("Speed of the ball: "))
-heading = float(input("Heading: "))
+heading = int(input("Heading: "))
 operational_time = float(input("Number of seconds: "))
 
 # place data gather values below
@@ -33,7 +33,7 @@ with SpheroEduAPI(toy_name) as bolt:
 
         orientation_data.append(bolt.get_orientation())
         gyroscope_data.append(bolt.get_gyroscope())
-        time.sleep(1)
+        time.sleep(0.01)
 
 print(location_data)
 print(velocity_data)
@@ -42,6 +42,7 @@ print(acceleration_data)
 x_loc = [loc_set["x"] for loc_set in location_data]
 y_loc = [loc_set["y"] for loc_set in location_data]
 # no z_loc
+dist = [(loc_set["x"] ** 2 + loc_set["y"] ** 2) ** 0.5 for loc_set in location_data]
 
 x_vel = [vel_set["x"] for vel_set in velocity_data]
 y_vel = [vel_set["y"] for vel_set in velocity_data]
@@ -51,7 +52,7 @@ x_accel = [accel_set["x"] for accel_set in acceleration_data]
 y_accel = [accel_set["y"] for accel_set in acceleration_data]
 z_accel = [accel_set["z"] for accel_set in acceleration_data]
 
-time_data = [moment-time_data[0] for moment in time_data]
+time_data = [moment - time_data[0] for moment in time_data]
 
 # x values graphed
 plt.plot(time_data, x_loc, label = "x")
@@ -62,9 +63,9 @@ plt.legend()
 plt.show()
 
 # y values graphed
-plt.plot(time_data, y_loc, label = "x")
-plt.plot(time_data, y_vel, label = "x-velocity")
-plt.plot(time_data, y_accel, label = "x-acceleration")
+plt.plot(time_data, y_loc, label = "y")
+plt.plot(time_data, y_vel, label = "y-velocity")
+plt.plot(time_data, y_accel, label = "y-acceleration")
 plt.xlabel("Time (s)")
 plt.legend()
 plt.show()
@@ -76,10 +77,15 @@ pitch = [orientation_set["pitch"] for orientation_set in orientation_data]
 yaw = [orientation_set["yaw"] for orientation_set in orientation_data]
 roll = [orientation_set["roll"] for orientation_set in orientation_data]
 
-pitch_vel = [gyro_set["pitch"] for gyro_set in gyroscope_data]
-yaw_vel = [gyro_set["yaw"] for gyro_set in gyroscope_data]
-roll_vel = [gyro_set["roll"] for gyro_set in gyroscope_data]
+gyro_x = [gyro_set["x"] for gyro_set in gyroscope_data]
+gyro_y = [gyro_set["y"] for gyro_set in gyroscope_data]
+gyro_z = [gyro_set["z"] for gyro_set in gyroscope_data]
 
-with open('functionalities.csv', 'a', newline='') as csvfile:
-    writer = csv.writer(csvfile)
-    # figure out a way to write in values into csv for easy comparison
+if (input("Sucessful? ").lower() == "y"):
+    with open('functionalities.csv', 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        # figure out a way to write in values into csv for easy comparison
+        for data_slice in zip(time_data, [speed] * len(time_data), x_loc, y_loc, dist, x_vel, y_vel, x_accel, y_accel, z_accel,
+                            pitch, yaw, roll, gyro_x, gyro_y, gyro_z):
+            print(data_slice)
+            writer.writerows([list(data_slice)])
